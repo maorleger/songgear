cd3202100d138f2cc221597a33201306aa381677
mleger <maor.leger@gmail.com>
2015-10-05 14:30:25 -0600

    first commit after git issues

---
+++ web/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.forms import ModelForm
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    name = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist)
+    video = models.URLField(null=True, blank=True)
+    chords_text = models.TextField(null=True, blank=True)
+    chords_url = models.URLField(null=True, blank=True)
+    tabs_text = models.TextField(null=True, blank=True)
+    tabs_url = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{0} by {1}".format(self.name, self.artist.name)
+
---
+++ web/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.views import generic
+from django.core.urlresolvers import reverse
+from .models import Song
+from .forms import EditForm
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+
+
+
+
+
+class IndexView(generic.ListView):
+    template_name = 'web/index.html'
+    context_object_name = 'song_list'
+
+    def get_queryset(self):
+        return Song.objects.order_by('name')
+
+
+class DetailView(generic.DetailView):
+    template_name = 'web/detail.html'
+    model = Song
+
+
+def edit(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'POST':
+        form = EditForm(request.POST, instance=song)
+        if form.is_valid():
+            song = form.save()
+        return HttpResponseRedirect(reverse('web:detail', args=(song.id,)))
+    else:
+        form = EditForm(instance=song)
+        print('we get')
+        print(song.id)
+    return render(request, 'web/edit.html', {'form': form, 'pk': pk})
