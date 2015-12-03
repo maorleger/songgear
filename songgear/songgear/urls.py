dedcbc2bcd0f16f758e09431b58f5abdcf76e8cd
mleger <maor.leger@gmail.com>
2015-12-03 16:51:43 -0700

    adding urls

--- songgear/songgear/urls.py
+++ songgear/songgear/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^/$', include(web_urls, namespace='web')),
     url(r'^/$', include('django.contrib.auth.urls')),
+                       
 )
