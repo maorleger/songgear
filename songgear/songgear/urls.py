56cb77d0211da15b0231706e38cafc72293dbb42
mleger <maor.leger@gmail.com>
2015-12-03 16:49:46 -0700

    adding urls

--- songgear/songgear/urls.py
+++ songgear/songgear/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 from web import urls as web_urls
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^web/', include(web_urls, namespace='web')),
-    url(r'^web/', include('django.contrib.auth.urls')),
+    url(r'^/$', include(web_urls, namespace='web')),
+    url(r'^/$', include('django.contrib.auth.urls')),
 )
